8e58f47cffe1f48ed209e001716e357da6b95ae7
snyderae13 <snyderae13@icloud.com>
2022-06-02 16:09:51 -0500

    testing port and get

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log('Server')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 5000
+const app = express()
+
+
+app.get('/', (req,res) => {
+    res.send('Hello')
+})
+
+app.listen(PORT, () => console.log(`server started on port ${PORT}`))
