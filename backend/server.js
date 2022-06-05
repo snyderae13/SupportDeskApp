096b2db7dd71242d7e9884240dd1a0dbd946565c
snyderae13 <snyderae13@icloud.com>
2022-06-05 14:27:57 -0500

    error handling and asyncHandler

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const {errorHandler}  = require('./middleware/errorMiddleware')
 const PORT = process.env.PORT || 5000
 const app = express()
 
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(errorHandler)
 
 app.get('/', (req,res) => {
     res.send('Hello')
