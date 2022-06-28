e16871e59d8ff796c8848ac33036ec2bad65165e
snyderae13 <snyderae13@icloud.com>
2022-06-28 18:26:14 -0500

    now able to view tickets based on user

--- frontend/src/pages/Tickets.jsx
+++ frontend/src/pages/Tickets.jsx
@@ -4,6 +4,7 @@ import {useSelector, useDispatch} from 'react-redux'
 import {getTickets, reset} from '../features/tickets/ticketSlice'
 import Spinner from '../components/Spinner'
 import { BackButton } from '../components/BackButton'
+import TicketItem from '../components/TicketItem'
 
 function Tickets() {
     const {tickets, isLoading, isSuccess} = useSelector((state) => state.tickets)
@@ -28,10 +29,22 @@ function Tickets() {
 
 
   return (
-    <div>
+    <>
+        <BackButton url='/'/>
         <h1>Tickets</h1>
+        <div className='tickets'>
+           <div className='ticket-headings'>
+               <div>Date</div>
+               <div>Product</div>
+               <div>Status</div>
+               <div></div>
+               </div> 
+               {tickets.map((ticket) =>(
+                   <TicketItem key={ticket._id} ticket={ticket}/>
+               ))}
+        </div>
 
-    </div>
+    </>
   )
 }
 
