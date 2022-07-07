383bff5aac912a8aea211b0ea48a4f58af8a88e1
snyderae13 <snyderae13@icloud.com>
2022-07-07 17:15:08 -0500

    ready to deploy

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
 const {errorHandler}  = require('./middleware/errorMiddleware')
@@ -21,5 +22,16 @@ app.get('/', (req,res) => {
 //Routes
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
+
+//Serve Frontend and set build folder as static 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
+} else {
+    app.get('/', (req,res) => {
+        res.status(200).json({message: 'Welcome to the Support Desk API'})
+    })
+}
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`))
